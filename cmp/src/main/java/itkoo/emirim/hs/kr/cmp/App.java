9d261e7db9b7f408ceba4580422e08968f8917f4
ITKOO <j4550118@e-mirm.hs.kr>
2017-10-26 09:33:40 +0900

    #1 First Commit FireBase Connect Sucess!

---
+++ cmp/src/main/java/itkoo/emirim/hs/kr/cmp/App.java
@@ -0,0 +1,13 @@
+package itkoo.emirim.hs.kr.cmp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
